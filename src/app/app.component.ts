482cef1a7ef187217d44e473287265d4db9a5546
Максим <41415269+KemtaTam@users.noreply.github.com>
2022-12-06 18:00:22 +0700

    prettier test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'team-project-wb';
+	title = 'team-project-wb'
+	lala() {
+		if (true) console.log('object')
+		if (true) console.log('object')
+		if (true) console.log('object')
+	}
 }
