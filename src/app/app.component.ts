578a513ac145ab4c78029facde5eae6d7a774981
Максим <41415269+KemtaTam@users.noreply.github.com>
2022-12-06 19:44:02 +0700

    add api service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { ApiService, IData0, IData1 } from './services/api.service';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core'
 
 @Component({
@@ -6,8 +8,39 @@ import { Component, OnInit } from '@angular/core'
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+	baseURL = "http://localhost:3001"
+	data0: IData0[] | null = null;
+	data1: IData1[] | null = null;
+	error = ''
+	isLoading = false
+
+	constructor(private apiService: ApiService) {}
+
 	ngOnInit(): void {
-		
+		this.apiService.getData0().subscribe({
+			next: (res) => {
+				this.isLoading = true
+				this.data0 = res;
+			},
+			error: (error) => {
+				this.error = error.message;
+			},
+			complete: () => {
+				this.isLoading = false;
+			},
+		})
+		this.apiService.getData1().subscribe({
+			next: (res) => {
+				this.isLoading = true
+				this.data1 = res;
+				debugger
+			},
+			error: (error) => {
+				this.error = error.message;
+			},
+			complete: () => {
+				this.isLoading = false;
+			},
+		})
 	}
-	
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { catchError, delay, throwError } from 'rxjs'
+
+export interface IData0 {
+	office_id: number
+	wh_id: number
+	qty: number
+}
+export interface IData1 extends IData0 {
+	dt_date: string
+}
+
+@Injectable({
+	providedIn: 'root'
+})
+export class ApiService {
+	baseURL = 'http://localhost:3001'
+	data0: IData0 | null = null
+	data1: IData1 | null = null
+
+	constructor(private http: HttpClient) {}
+
+	getData0() {
+		return this.http.get<IData0[]>(`${this.baseURL}/data0`).pipe(
+			delay(500),
+			catchError((error) => {
+				console.log('Error: ', error.message);
+				return throwError(() => error);
+			})
+		)
+	}
+
+	getData1() {
+		return this.http.get<IData1[]>(`${this.baseURL}/data1`).pipe(
+			delay(500),
+			catchError((error) => {
+				console.log('Error: ', error.message);
+				return throwError(() => error);
+			})
+		)
+	}
+}
