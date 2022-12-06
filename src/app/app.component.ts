51a87cd90b3107cfa03eb0dc04bb1c06480af319
VDaily <workingEmail2207@gmail.com>
2022-12-06 14:09:18 +0300

    test#1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,18 @@ import { Component } from '@angular/core'
 export class AppComponent {
 	title = 'team-project-wb'
 	lala() {
-		if (true) console.log('object')
+		if (true) console.log('object');
+    if(true) {
+      let map = new Map();
+      let array = [1,3,3,5,6,7,9,8,9];
+      array.forEach(elem => {
+        if(!map.has(elem)) {
+          map.set(elem, {
+            data: []
+          })
+        }
+      })
+    }
 		if (true) console.log('object')
 		if (true) console.log('object')
 	}
