9c9cb003fec3be1a5ac4e86149b920fb32bd951a
VDaily <workingEmail2207@gmail.com>
2022-12-06 17:42:14 +0300

    Starting point. Configured the table module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { TableModule } from './modules/table/table.module'
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'table',
+		loadChildren: () => TableModule
+	}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/table/table.module.ts
+++ src/app/modules/table/table.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { TableComponent } from './table/components/table.component'
+import { TableRoutingModule } from './table.routing.module'
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [TableComponent],
+	imports: [CommonModule, TableRoutingModule]
 })
-export class TableModule { }
+export class TableModule {}
