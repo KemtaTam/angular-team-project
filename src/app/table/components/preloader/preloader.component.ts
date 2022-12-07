2966eb376e94b16a076b6174de7ffedd57df64ba
VDaily <workingEmail2207@gmail.com>
2022-12-07 23:10:23 +0300

    Changed the image of the preloader

--- src/app/table/components/preloader/preloader.component.ts
+++ src/app/table/components/preloader/preloader.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-preloader',
-  templateUrl: './preloader.component.html',
-  styleUrls: ['./preloader.component.scss']
+	selector: 'app-preloader',
+	templateUrl: './preloader.component.html',
+	styleUrls: ['./preloader.component.scss']
 })
 export class PreloaderComponent {
-
+	preloaderObj = {
+		src: './assets/images/preloader.gif',
+		alt: 'Изображение загрузки контента'
+	}
 }
